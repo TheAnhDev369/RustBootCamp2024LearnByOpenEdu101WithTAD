1b61b04fed342a489e69bbced91e131e9e114f83
AnhDev <nguyentheanhayn279@gmail.com>
2024-10-27 00:38:33 +0700

    Initial commit 2

---
+++ hello-world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+D:\AlllMyCode\Code\Crypto\Rust\hello-world\target\debug\deps\hello_world.exe: src/main.rs
+
+D:\AlllMyCode\Code\Crypto\Rust\hello-world\target\debug\deps\hello_world.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+D:\AlllMyCode\Code\Crypto\Rust\hello-world\target\debug\hello-world.exe: D:\AlllMyCode\Code\Crypto\Rust\hello-world\src\main.rs
