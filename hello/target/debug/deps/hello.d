1dad47c1cdb8d456a828acc951ec0fe828060c87
AnhDev <nguyentheanhayn279@gmail.com>
2024-10-27 00:37:40 +0700

    Initial commit

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+D:\AlllMyCode\Code\Crypto\Rust\hello\target\debug\deps\hello.exe: src/main.rs
+
+D:\AlllMyCode\Code\Crypto\Rust\hello\target\debug\deps\hello.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+D:\AlllMyCode\Code\Crypto\Rust\hello\target\debug\hello.exe: D:\AlllMyCode\Code\Crypto\Rust\hello\src\main.rs
